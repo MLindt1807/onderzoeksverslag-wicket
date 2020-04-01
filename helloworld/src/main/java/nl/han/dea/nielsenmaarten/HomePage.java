d8bb114407553dda3cfd07a87ad81604f7eda5e3
MLindt1807 <47355174+MLindt1807@users.noreply.github.com>
2020-04-01 11:22:26 +0100

    hello world werkt

---
+++ helloworld/src/main/java/nl/han/dea/nielsenmaarten/HomePage.java
@@ -0,0 +1,17 @@
+package nl.han.dea.nielsenmaarten;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+
+
+	public HomePage(){
+
+		add(new Label("hello", "alloo wereld"));
+
+		// TODO Add your page's components here
+
+	}
+}
---
+++ wicket-onderzoek/src/main/java/nl/han/dea/nielsenmaarten/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package nl.han.dea.nielsenmaarten;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+public class HelloWorldApplication extends WebApplication {
+    public HelloWorldApplication() {
+    }
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    @Override
+    public Class getHomePage() {
+        return HelloWorld.class;
+    }
+}
